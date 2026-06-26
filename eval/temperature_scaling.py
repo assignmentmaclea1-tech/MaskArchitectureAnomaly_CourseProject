8d4b9732ae0a5eb880b57bedbee3443cf88cbcc1
assignmentmaclea1-tech <assignmentmaclea1@gmail.com>
2026-06-26 23:23:36 +0200

    Update temperature_scaling.py

--- eval/temperature_scaling.py
+++ eval/temperature_scaling.py
@@ -2,6 +2,18 @@ import torch
 from torch import nn, optim
 from torch.nn import functional as F
 
+'''
+"""
+Questo modulo implementa la Temperature Scaling, una tecnica di calibrazione usata per migliorare le prestazioni del classifier.
+La classe incapsula un modello già addestrato e introduce un parametro scalare apprendibile (temperature) che viene 
+usato per ridimensionare i logits in output.
+Nel forward, il modello produce i logits e questi vengono divisi per la temperatura, modificando la “confidenza” 
+delle predizioni senza alterare le classi previste.
+Nel caso della segmentazione, la scaling viene applicata direttamente ai logits 
+pixel-wise senza necessità di adattamenti aggiuntivi.
+"""
+'''
+
 class ModelWithTemperature(nn.Module):
     def __init__(self, model):
         super(ModelWithTemperature, self).__init__()
